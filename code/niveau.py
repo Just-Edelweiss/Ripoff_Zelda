c8e27795e97f4fb039e89c648ca5f0eead0232c1
Just_Edelweiss <atome2carbone@hotmail.com>
2023-10-15 23:11:05 +0200

    map display

--- code/niveau.py
+++ code/niveau.py
@@ -17,14 +17,15 @@ class Niveau:
         self.create_map()
 
     def create_map(self):
-        for index_ligne, ligne in enumerate(map):
+        """        for index_ligne, ligne in enumerate(map):
             for index_colonne, colonne in enumerate(ligne):
                 x = index_colonne * tilesize
                 y = index_ligne * tilesize
                 if colonne == 'x':
                     Tiles((x, y), [self.sprites_visible, self.sprites_barriere])
                 if colonne == 'p':
-                    self.perso = Perso((x, y), [self.sprites_visible], self.sprites_barriere)
+                    self.perso = Perso((x, y), [self.sprites_visible], self.sprites_barriere)"""
+        self.perso = Perso((448, 96), [self.sprites_visible], self.sprites_barriere)
 
     def run(self):
         self.sprites_visible.draw_custom()
@@ -36,8 +37,16 @@ class YSortCameraGroup(pyg.sprite.Group):
 
         super().__init__()
         self.affiche_surface = pyg.display.get_surface()
+        self.offset = pyg.math.Vector2()
+        
+
+        self.sol_surf = pyg.image.load('assets/Zeldouille_map.png').convert()
+        self.sol_rect = self.sol_surf.get_rect(topleft = (0, 0))
 
     def draw_custom(self):
+        pos_offset_sol = self.sol_rect.topleft
+        self.affiche_surface.blit(self.sol_surf, pos_offset_sol)
+
         for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
             self.affiche_surface.blit(sprite.image, sprite.rect)
         
